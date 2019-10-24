39aab62052de164550a392d528d9b9fe7f4f8d29
Siddharthan <siddharthan@hakunamatata.in>
2019-10-24 19:03:24 +0530

    Spelling mistake

--- app/src/main/java/com/hms/utils/Constants.kt
+++ app/src/main/java/com/hms/utils/Constants.kt
@@ -4,7 +4,7 @@ interface Constants {
 
     interface ErrorCode {
         companion object {
-            val NAME_ERROR = "Name should be minimum 2 letters and mximum of 15 letters"
+            val NAME_ERROR = "Name should be minimum 2 letters and maximum of 15 letters"
             val MAIL_ID_ERROR = "Invalid email"
         }
     }
